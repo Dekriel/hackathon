7e47e33036697d865cb4bfd9b11b6c490171f019
Dekriel <92822789+Dekriel@users.noreply.github.com>
2022-12-16 14:48:09 +0000

    backend update 1

--- backend.py
+++ backend.py
@@ -1,7 +1,49 @@
 from flask import *
+import json
+import chess
 
+def filter_moves_on_piece(moves, square):
+    moves_ls = [str(move) for move in moves]
+    new = []
+    valid = []
+    for i in moves_ls:
+        j = chess.BaseBoard().piece_at(chess.parse_square(i[:2]))
+        new.append(str(j) + i[-2:])
+        for k in new:
+            print(k)
+            if square in k:
+                valid.append(k)
 
+    return json.dumps({moves: valid})
+
+# create the app
 app = Flask(__name__)
 
 
-@app.route('/')
+# start the board
+board = chess.Board()
+
+@app.route('/', methods=['POST', 'GET'])
+def homepage():
+    return render_template("chess.html")
+
+@app.route('/moves/<square>')
+def get_moves(square):
+    legal_moves = board.legal_moves
+    return filter_moves_on_piece(legal_moves, square)
+
+@app.route('/state')
+def state():
+    return json.dumps({'board': str(board)})
+
+@app.route('/move/{squareStart}-{squareEnd}')
+def make_move(squareStart, squareEnd):
+    try:
+        board.push(chess.Move.from_uci(f"{squareStart.lower()}{squareEnd.lower()}"))
+    except Exception as e:
+        return 500, f"{squareStart.lower()}{squareEnd.lower()} is an invalid uci"
+    return 200, state()
+
+
+if __name__ == '__main__':
+    app.run()
